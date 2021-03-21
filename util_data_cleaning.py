7fdc5182a2325039a850d3d2962b3fb38e26264a
ElifKarakutukDinc <elifkaraktk@gmail.com>
2021-03-21 17:52:08 +0100

    deleted find and replace first string function in util_data_cleaning.py

--- util_data_cleaning.py
+++ util_data_cleaning.py
@@ -2,58 +2,6 @@ import pandas as pd
 import re
 
 
-def extract_first_string(df, column_to_be_used, column_to_be_created):
-    """
-    This function gets a Python Pandas dataframe and extracting the middle string from name column in the dataframe. And Replacing values in new column.
-    :param df: Dataframe to be analyze
-    :param column_to_be_used: The column is to be used to extract the middle string to the new column.
-    :param column_to_be_created: The column is to be created from "column_to_be_used"
-    :return: Returning Python Pandas dataframe.
-    """
-    try:
-
-        df[column_to_be_created] = df[column_to_be_used].str.extract("([A-Za-z]+)\.")
-
-        df[column_to_be_created].replace(
-            [
-                "Mlle",
-                "Mme",
-                "Ms",
-                "Dr",
-                "Major",
-                "Lady",
-                "Countess",
-                "Jonkheer",
-                "Col",
-                "Rev",
-                "Capt",
-                "Sir",
-                "Don",
-            ],
-            [
-                "Miss",
-                "Miss",
-                "Miss",
-                "Officer",
-                "Officer",
-                "Mrs",
-                "Mrs",
-                "Mr",
-                "Officer",
-                "Officer",
-                "Officer",
-                "Mr",
-                "Mr",
-            ],
-            inplace=True,
-        )
-
-        return df
-
-    except Exception as e:
-        print("Error at df_first_look function: ", str(e))
-        return df
-
 
 def df_numeric_column_filler_with_aggregated_data(
     df, group_list, column_to_be_filled, aggregation_method="median"
@@ -69,7 +17,7 @@ def df_numeric_column_filler_with_aggregated_data(
     try:
         df_dummy = df.groupby(group_list)[column_to_be_filled].transform(
             aggregation_method
-        )  # to see age group's aggregation_method
+        )  
 
         df.loc[
             df[column_to_be_filled].isnull(), column_to_be_filled
